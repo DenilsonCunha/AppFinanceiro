d6522a575c12af7d02b38b3b546ae09e68542ec6
Denilson Luciano <denilsonluciano6@gmail.com>
2021-11-03 09:01:00 -0300

    criando pages e rotas

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
 export default function App() {
  return (
-   <View/>
+     <View>
+       <StatusBar backgroundColor="#131313" barStyle="light-content" />
+     </View>
   );
 }
